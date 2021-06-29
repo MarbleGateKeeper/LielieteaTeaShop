696fa2d5af6f7c0dc1c45f385eab289abf34ec20
白门守望者 <marblegatekeeper@outlook.com>
2021-06-30 07:40:35 +0800

    Bop
    Bop

--- src/main/java/love/marblegate/lielietea/LIeLieTea.java
+++ src/main/java/love/marblegate/lielietea/LIeLieTea.java
@@ -3,12 +3,9 @@ package love.marblegate.lielietea;
 import love.marblegate.lielietea.registry.ItemRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod("lielietea")
 public class LIeLieTea {
-    public static final Logger LOGGER = LogManager.getLogger();
     public LIeLieTea(){
         ItemRegistry.ITEM.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
--- src/main/java/love/marblegate/lielietea/itemgroup/ModGroup.java
+++
@@ -1,9 +0,0 @@
-package love.marblegate.lielietea.itemgroup;
-
-import net.minecraft.item.ItemGroup;
-
-public class ModGroup {
-    public static final ItemGroup drinkGroup = new DrinkGroup();
-    public static final ItemGroup ingredientGroup = new IngredientGroup();
-    public static final ItemGroup productionGroup = new ProductionGroup();
-}
